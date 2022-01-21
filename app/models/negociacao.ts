7f8c5bafde0644341fea24d811588267834fd0f1
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-01-21 16:13:55 -0300

    comecando com Typescript

---
+++ app/models/negociacao.ts
@@ -0,0 +1,16 @@
+export class Negociacao {
+  constructor(
+    public _data: Date, 
+    private _quantidade: number, 
+    private _valor: number
+  ) {}
+
+  get data(): Date {
+    const data = new Date(this._data.getTime());
+    return data
+  }
+
+  get volume(): number {
+    return this._quantidade * this._valor
+  }
+}
---
+++ dist/js/models/negociacao.js
@@ -0,0 +1,14 @@
+export class Negociacao {
+    constructor(_data, _quantidade, _valor) {
+        this._data = _data;
+        this._quantidade = _quantidade;
+        this._valor = _valor;
+    }
+    get data() {
+        const data = new Date(this._data.getTime());
+        return data;
+    }
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+}
